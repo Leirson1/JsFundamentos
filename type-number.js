778f2bce964fb604b70d2a76fd47ff889e463479
Leirson Manoel de Melo <leirson@bb.com.br>
2022-06-22 15:29:21 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,22 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperação = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperação);
+
+//NaN -> Not A Number (Não é um número)
+
+const Alura = "Alura";
+console.log(Alura * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,23 @@
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = 'senhaSuperSegura456!';
+const stringDeNumeros = '34567';
+const citacao = 'Meu nome é ';
+const meuNome = 'Leirson'
+
+//template string ou template literal
+
+// concatenação (+)
+console.log(citacao + meuNome);
+
+//outros
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
