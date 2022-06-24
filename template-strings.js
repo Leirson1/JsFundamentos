1a2dc3b35ca74e77689f7c7234c03dfc83f8445d
Leirson Melo <Leirson@hotmail.com>
2022-06-24 10:28:19 -0300

    feat: :zap: Aula 4
    Arquivos da aula 4

--- template-strings.js
+++ template-strings.js
@@ -1,11 +1,15 @@
+//aula 4
+
 const nome = "Ju";
-const idade = 2021-1981;
-const cidadeDeNascimento = "São Paulo";
+const idade = 2022-1983;
+const cidadeDeNascimento = "Curitiba";
 
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
 
+// usar a ` ` (crase) no lugar das ' ' (aspas)
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
 
 console.log(apresentacao)
+
 
 
